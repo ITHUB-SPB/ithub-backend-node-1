9f26f26bf74e7a059d366f87c2f94ad1028b65ba
Трофимов Алексей <a.trofimov@domain.local>
2026-09-12 11:05:57 +0300

    exercise

---
+++ src/readable-exercise.js
@@ -0,0 +1,13 @@
+/*
+  Exercise
+
+  Write a function that receives a stream and counts the number of words in the stream.
+  Be careful that in a chunk you might receive a word that is currently truncated, so
+  try to figure out how to reconcile words spanning 2 chunks.
+*/
+
+export default async function countWords (srcStream) {
+  // ... implement here the logic to count the number of words in the stream
+  // ... return the actual number of words when the stream is finished
+  return 0
+}
