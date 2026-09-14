fe8d2c5dde64a83c42ab4108dc761a2b123363df
Бельшов Киприан <k.belshov@domain.local>
2026-09-14 15:39:56 +0300

    finish 1.1

--- src/fs/snapshot.ts
+++ src/fs/snapshot.ts
@@ -1,15 +1,31 @@
 import path from 'node:path'
-import { readdir, readFile } from 'node:fs/promises'
+import { readdir, readFile, writeFile, stat } from 'node:fs/promises'
 
 const snapshot = async () => {
-  const files = await readdir('./data', { withFileTypes: true, recursive: true })
-  const promises = files.map(async (file) => {
-    const fullPath = path.join(file.parentPath, file.name)
-    const data = file.isFile() ? await (await readFile(fullPath)).toString('base64') : null
-    return { path: fullPath, ...(data && { contents: data }) }
-  })
-  const results = await Promise.all(promises)
-  console.log(results)
+  try {
+    const workspace = 'data'
+    const files = await readdir(workspace, { withFileTypes: true, recursive: true })
+    const promises = files.map(async (file) => {
+      const fullPath = path.join(file.parentPath, file.name)
+      const data = file.isFile() ? await (await readFile(fullPath)).toString('base64') : null
+      const filetype = file.isFile() ? "file" : "directory"
+      return {
+        path: path.relative(workspace, fullPath),
+        type: filetype,
+        ...(file.isFile() && { size: (await stat(fullPath)).size }),
+        ...(file.isFile() && { contents: data })
+      }
+    })
+    const results = {
+      rootPath: workspace,
+      entries: await Promise.all(promises)
+    }
+    writeFile('snapshot.json', JSON.stringify(results, null, 2))
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed')
+    }
+  }
 };
 
 await snapshot();
