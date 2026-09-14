9dfcf76482735e49b7994d21e389ea221409dda3
Art fisher <yo.email@example.com>
2026-09-14 16:24:42 +0300

    1

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises'
+import path from 'node:path';
+
 const restore = async () => {
-  // TODO
+  try{
+    const result = JSON.parse(await fs.readFile('./src/fs/snapshot.json', 'utf-8'))
+    for(const filename of result){
+      const failpath = filename.path
+      if(path.extname(failpath) === ''){
+        await fs.writeFile('')
+        onsole.log(f)
+      }
+      else{
+        await fs.writeFile
+        onsole.log(g)
+      }
+    }
+  }catch (error){
+    console.error(error)
+  }
 };
 
 await restore();
--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -4,17 +4,34 @@ import path from 'node:path'
 const snapshot = async () => {
   try {
 
-    const result = await fs.readdir('./data', { withFileTypes: true })
-    let result2
+    const result = await fs.readdir('./data', { withFileTypes: true, recursive: true })
+    let fake_result2 = []
+
+
     for (const filename of result) {
-      if(filename.isDirectory()){
-        result2 = await fs.readdir(`./data/${filename.name}`, { withFileTypes: true })
+      if (filename.isDirectory()) {
+        fake_result2.push({ "path": filename.name, "type": "directory" })
+      } else {
+        const filePath = path.join(filename.parentPath, filename.name)
+
+        fake_result2.push({ 
+          "path": filePath, 
+          "type": "file", 
+          "size":  (await fs.stat(filePath)).size, 
+          "content":  await fs.readFile(filePath, 'base64')
+        })
       }
+
     }
-    console.log(result, result2)
+    try{
+      await fs.writeFile('./src/fs/snapshot.json', JSON.stringify(fake_result2, null, 2))
+    }catch (error){
+      console.error(error)
+    }
   } catch (error) {
     console.error(error)
   }
+
 };
 
 await snapshot();
