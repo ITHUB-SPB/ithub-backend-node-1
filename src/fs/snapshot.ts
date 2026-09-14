965bc620bc73e31f25d2cae8f3ab77443bb41d7b
Бельшов Киприан <k.belshov@domain.local>
2026-09-14 12:11:45 +0300

    start implementing 1.1

--- src/fs/snapshot.ts
+++ src/fs/snapshot.ts
@@ -1,5 +1,15 @@
+import path from 'node:path'
+import { readdir, readFile } from 'node:fs/promises'
+
 const snapshot = async () => {
-  // TODO
+  const files = await readdir('./data', { withFileTypes: true, recursive: true })
+  const promises = files.map(async (file) => {
+    const fullPath = path.join(file.parentPath, file.name)
+    const data = file.isFile() ? await (await readFile(fullPath)).toString('base64') : null
+    return { path: fullPath, ...(data && { contents: data }) }
+  })
+  const results = await Promise.all(promises)
+  console.log(results)
 };
 
 await snapshot();
